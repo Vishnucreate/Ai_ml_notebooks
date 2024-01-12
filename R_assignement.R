8000a8c54cd7754718a6977064f6490856377889
Vishnucreate <65901404+Vishnucreate@users.noreply.github.com>
2024-01-12 13:10:42 -0500

    Add files via upload

---
+++ R_assignement.R
@@ -0,0 +1,157 @@
+1+(2+3)
+(4^3) + 3^(2+1)
+((4+3)*(2+1))**(1/2)
+((1+2)/(3+4))**(2)
+(2+3) - 4
+(2+3)*4
+(2/3)*4
+(2^3)**4
+(1+(2*(3)**4))/((5/6)-7)
+(0.25 - 0.2)/((0.2)*(1-0.2)/100)**1/2
+
+number1 = 2
+number2 = 3
+number3 =  4
+number4  = 5
+
+
+result = number1 * number2 * number3 * number4
+
+print(result)
+
+
+
+
+data(rivers)
+tail(rivers,1)
+
+
+
+data = UsingR::exec.pay
+
+largest_value <- max(exec.pay)
+
+
+largest_value
+
+
+mean_exec_pay = mean(exec.pay)
+
+
+trimmed_mean_exec_pay = mean(exec.pay, trim = 0.10)
+
+difference = mean_exec_pay - trimmed_mean_exec_pay
+
+
+cat("Mean of exec.pay:", mean_exec_pay, "\n")
+cat("Trimmed Mean of exec.pay:", trimmed_mean_exec_pay, "\n")
+cat("Difference between the means:", difference, "\n")
+
+
+data(Orange)
+
+
+name_of_variables = names(Orange)
+name_of_variables
+
+
+
+average_age_of_oranges =  mean(Orange$age)
+
+
+average_age
+
+
+
+data(Orange)
+
+largest_circumference_Tree <- max(Orange$circumference)
+
+
+largest_circumference_Tree
+
+
+
+p = c(2, 3, 5, 7, 11, 13, 17, 19)
+
+
+length_p = length(p)
+
+
+length_p
+
+gas = c(65311, 65624, 65908, 66219, 66499, 66821, 67145, 67447)
+
+
+mileage_difference = diff(gas)
+
+mean_gas = mean(gas)
+
+mean_difference_gas = mean(diff(gas))
+mean_gas
+mean_difference_gas
+
+
+# The difference between mean(gas) and mean(diff(gas)) is the former gives just the value mean of
+# while the latter mean(diff(gas)) gives the mean of difference of the gas
+
+mean_mileage_difference <- mean(mileage_difference)
+
+
+cat("Gas Mileage:", gas, "\n")
+cat("Mileage Differences:", mileage_difference, "\n")
+cat("Mean Gas Mileage:", mean_gas, "\n")
+cat("Mean Mileage Differences:", mean_mileage_difference, "\n")
+
+
+value = c(25, 4, 10, 8)
+
+
+squared_value = x^2
+
+squared_value
+
+value_minus_6  =  x - 6
+
+value_minus_6
+
+
+squared_value_9 = (x - 9)^2
+
+squared_value_9
+
+pattern_1 = rep("a", times = 5)
+pattern_1
+
+pattern_2 = seq(from =1 , to = 99 , by =2)
+pattern_2
+
+pattern_3 = rep(1:3 , each = 3)
+pattern_3
+
+pattern_4 = rep(1:3 , times = c(3,2,1))
+pattern_4
+
+pattern_5  = c((1:5) , rev(1:4))
+pattern_5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
